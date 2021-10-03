09da5d3abcd8ce371fd70a4e509b172f9e3d9b3d
mstsi345 <strangesky010@gmail.com>
2021-10-03 20:28:10 +0800

    Correct the settings of API key.

--- yt_concate/main.py
+++ yt_concate/main.py
@@ -4,12 +4,12 @@ import urllib.request
 import json
 from yt_concate.settings import API_KEY
 
-print(API_KEY)
+
 CHANNEL_ID = 'UCKSVUHI9rbbkXhvAXK-2uxA'
 
 
 def get_all_video_in_channel(channel_id):
-    api_key = 'AIzaSyBLZIoeFMJmyeopHJPOfntJWSi9RGaZ9Oc'
+    api_key = API_KEY
 
     base_video_url = 'https://www.youtube.com/watch?v='
     base_search_url = 'https://www.googleapis.com/youtube/v3/search?'
