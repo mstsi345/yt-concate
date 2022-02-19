441b6ff2514355d7bb1fe4b175fe21dc0ab6f2c5
mstsi345 <strangesky010@gmail.com>
2022-02-19 16:51:39 +0800

    Modify error during reading caption

--- yt_concate/pipeline/steps/download_captions.py
+++ yt_concate/pipeline/steps/download_captions.py
@@ -1,43 +1,49 @@
 import os
+import time
 
 from pytube import YouTube
 
 from .step import Step
 from .step import StepException
-import time
+
 
 class DownloadCaptions(Step):
     def process(self, data, inputs, utils):
         # download the caption by pytube
         start = time.time()
-        for url in data:
-            print('downloading caption for', url)
-            if utils.caption_file_exists(url):
+        for yt in data:
+            print('downloading caption for', yt.id)
+            if utils.caption_file_exists(yt):
                 print('found existing caption file')
                 continue
 
+            if yt.id == 'WPKYpZeFv-0':
+                continue
+
             try:
-                source = YouTube(url)
+                source = YouTube(yt.url)
                 en_caption = source.captions.get_by_language_code('a.en')
                 en_caption_convert_to_srt = (en_caption.generate_srt_captions())
                 # print(en_caption_convert_to_srt)
             except KeyError:
-                print('KeyError for downloading caption for', url)
+                print('KeyError for downloading caption for', yt.url)
                 continue
             except AttributeError:
-                print('AttributeError for downloading caption for', url)
+                print('AttributeError for downloading caption for', yt.url)
                 continue
             # except KeyError, AttributeError:
             #     print('Error for downloading caption for', url)
             #     continue
 
             # save the file
-            text_file = open(utils.get_caption_filepath(url), "w", encoding='utf-8')
+            text_file = open(utils.get_caption_filepath(yt.url), "w", encoding='utf-8')
             text_file.write(en_caption_convert_to_srt)
             text_file.close()
 
         end = time.time()
         print('took', end - start, 'seconds')
+
+        return data
 
 
 #========================================================
