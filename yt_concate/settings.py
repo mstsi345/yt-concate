6c5c0e72840f4513a0929605ca31fe17195354a8
mstsi345 <strangesky010@gmail.com>
2022-02-26 16:25:15 +0800

    Finished editing videos and producing output.

--- yt_concate/settings.py
+++ yt_concate/settings.py
@@ -9,3 +9,5 @@ API_KEY = os.getenv('API_KEY')  # get environment variables
 DOWNLOADS_DIR = 'downloads'
 VIDEOS_DIR = os.path.join(DOWNLOADS_DIR, 'videos')
 CAPTIONS_DIR = os.path.join(DOWNLOADS_DIR, 'captions')
+OUTPUTS_DIR = 'outputs'
+
